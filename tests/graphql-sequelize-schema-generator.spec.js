a572f503d45cb1f3f7dd46d25a0f565d7d99fb13
Paulo Vitor Magacho da Silva <pvmagacho@gmail.com>
2019-07-26 00:21:32 -0300

    Updated snapshot

--- tests/graphql-sequelize-schema-generator.spec.js
+++ tests/graphql-sequelize-schema-generator.spec.js
@@ -1,17 +1,16 @@
-const {
-  GraphQLSchema
-} = require('graphql')
 const graphqlSchemaGenerator = require('../src/graphql-sequelize-schema-generator').generateSchema
 const graphqlSchemaGenerator_build = require('../dist/graphql-sequelize-schema-generator').generateSchema
-const stringifier = require('stringifier')({maxDepth: 10, indent: '  '})
+const stringifier = require('stringifier')({ maxDepth: 10, indent: '  ' })
 const models = require('./models')
 
 it('outputs the correct schema', () => {
   const schema = graphqlSchemaGenerator(models)
-  expect(stringifier(schema)).toMatchSnapshot()
+  expect(stringifier(schema.query.getFields())).toMatchSnapshot()
+  expect(stringifier(schema.mutation.getFields())).toMatchSnapshot()
 })
 
 it('outputs the correct schema (build version)', () => {
   const schema_build = graphqlSchemaGenerator_build(models)
-  expect(stringifier(schema_build)).toMatchSnapshot()
+  expect(stringifier(schema_build.query.getFields())).toMatchSnapshot()
+  expect(stringifier(schema_build.mutation.getFields())).toMatchSnapshot()
 })
